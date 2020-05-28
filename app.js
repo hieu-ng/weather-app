e5ab87ae4c1c98e2a6c6ad5218788c1012d51e03
hieu-ng <hieutnguyen06@gmail.com>
2020-05-28 20:46:31 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const api = {
   key: "afaf9f8d48cff6cafd32e23220bcfdbf",
-  base: "https://api.openweathermap.org/data/2.5/"
+  baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -13,7 +13,7 @@ function setQuery(evt) {
 }
 
 function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  fetch(`${api.baseurl}/weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     })
